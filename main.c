cf4bd3b1f71c9f5beafe3685574265d772b4c86f
l4meeeer <ilya98i13@gmail.com>
2017-02-27 00:58:33 +0700

    make

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "mylib/mylib.h"
+
+int main(){
+
+	printf("max(4,3) = i% \n", max(5,2));
+	return 0;
+}
---
+++ mylib/mylib.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "mylib.h"
+
+void test() {
+
+}
+
+int max(int a, int b){
+	printf("Hello\n");
+	return a >= b ? a : b; // возвращаем а или б
+}
